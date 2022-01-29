7c24bfb537c5e9742299b6aa9c8c5b0178309a4d
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-01-29 16:02:44 +0800

    self testing

--- src/test.ts
+++ src/test.ts
@@ -1,16 +1,29 @@
-import { check } from './utils';
+import { check, loadDict } from './utils';
 import { Context } from './Context';
+import inquirer from 'inquirer';
 
 async function main() {
-  const expected = 'attempt';
+  const wordList = await loadDict(5);
 
-  const context = new Context(expected.length, async (attempt) =>
-    check(attempt, expected)
-  );
+  const { count } = await inquirer.prompt<{ count: number }>({
+    type: 'number',
+    name: 'count',
+    message: 'Count of test cases',
+    default: 10,
+  });
 
-  const result = await context.guess();
+  for (let i = 0; i < count; i++) {
+    const index = Math.floor(Math.random() * wordList.length);
+    const expected = wordList[index];
+    console.log(`expected = ${expected}`);
+    const context = new Context(expected.length, async (attempt) =>
+      check(attempt, expected)
+    );
+    const result = await context.guess();
 
-  console.log('result =', result);
+    console.log(`result = ${result}`);
+    console.log('------');
+  }
 }
 
 main();
